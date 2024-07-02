f7c8c9e40113d6bba740c243a5ca3f0dad59d08c
Arif <arifhida1647@gmail.com>
2024-07-02 07:15:40 +0700

    update sql

--- ci4 (2).sql
+++ ci4 (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2024 at 02:42 AM
+-- Generation Time: Jul 02, 2024 at 02:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -145,6 +145,22 @@ INSERT INTO `laundry_items` (`item_id`, `item_name`, `waktu`, `category`, `descr
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
@@ -153,6 +169,7 @@ CREATE TABLE `orders` (
   `user_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL,
   `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `order_address` varchar(30) DEFAULT NULL,
   `status` enum('Pending','In Progress','Completed','Cancelled') DEFAULT 'Pending',
   `total_amount` decimal(10,2) DEFAULT NULL,
   `payment_proof_path` varchar(255) DEFAULT NULL
@@ -162,9 +179,10 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `user_id`, `item_id`, `order_date`, `status`, `total_amount`, `payment_proof_path`) VALUES
-(23, 2, 22, '2024-06-20 06:50:10', 'In Progress', 40000.00, 'https://i.ibb.co.com/vw6f8GR/message-Image-1718858957549.jpg'),
-(25, 2, 21, '2024-06-29 14:17:51', 'In Progress', 30000.00, 'https://i.ibb.co.com/mJSC64z/online-marketing-h-Igeo-Qj-S-i-E-unsplash.jpg');
+INSERT INTO `orders` (`order_id`, `user_id`, `item_id`, `order_date`, `order_address`, `status`, `total_amount`, `payment_proof_path`) VALUES
+(23, 2, 22, '2024-06-20 06:50:10', NULL, 'In Progress', 40000.00, 'https://i.ibb.co.com/vw6f8GR/message-Image-1718858957549.jpg'),
+(25, 2, 21, '2024-06-29 14:17:51', NULL, 'In Progress', 30000.00, 'https://i.ibb.co.com/mJSC64z/online-marketing-h-Igeo-Qj-S-i-E-unsplash.jpg'),
+(27, 2, 21, '2024-07-01 00:08:00', 'jl kesatrian', 'Pending', 30000.00, NULL);
 
 -- --------------------------------------------------------
 
@@ -228,6 +246,12 @@ ALTER TABLE `laundry_items`
   ADD PRIMARY KEY (`item_id`);
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
@@ -272,10 +296,16 @@ ALTER TABLE `laundry_items`
   MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
